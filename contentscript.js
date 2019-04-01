f539cc52c8c1535e2026795b11efc1c8bef26c05
Gino Valente <gino.valente3@gmail.com>
2019-04-01 13:55:38 -0700

    Changed container search string. Marked bug where it doesn't find all images

--- contentscript.js
+++ contentscript.js
@@ -1,6 +1,6 @@
 let images = new Map();
 const GDOCS_IMG_OBJECT_CLASS = "kix-embeddedobject-image";
-const GDOCS_IMG_CONTAINER_CLASS = "goog-inline-block";
+const GDOCS_IMG_CONTAINER_CLASS = "kix-embeddedobject-view";
 
 // Get the image URL from the GDOCS_IMG_OBJECT_CLASS
 function getImageURL(image) {
@@ -17,6 +17,7 @@ function insertImageLinkAnchor(image) {
 
 // Find the images and store the object and image source
 function findImageEmbeds() {
+  //TODO: 1
   let images = document.getElementsByClassName(GDOCS_IMG_OBJECT_CLASS);
   for (let image of images) {
     insertImageLinkAnchor(image);
@@ -24,4 +25,9 @@ function findImageEmbeds() {
 }
 
 // Initiate after all assets have loaded
-window.addEventListener("load", () => findImageEmbeds(), false);
+window.addEventListener("load", findImageEmbeds(), false);
+
+//TODO 1: Not finding images when they are in bulleted lists or tables
+// standalone: https://docs.google.com/document/d/1T570Ldo_PcOhL4jNHDOAc4TUnGct2cJw-D7cba6aYec/edit
+// lists: https://docs.google.com/document/d/1qZQEd1cDHPBxGHZ5I15kFIoyxjFdn7cBlEVxYoUZgnU/edit
+// tables: https://docs.google.com/document/d/18gKe4HfBRnL75ouNO5S5MP7xt72QF_xJjo0fmEjuvw0/edit

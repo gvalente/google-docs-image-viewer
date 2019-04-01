c2290dcc41984957e09df0a3d9f100d94d1ace83
Gino Valente <gino.valente3@gmail.com>
2019-04-01 10:47:11 -0700

    removed background.js, cleaned up image handling

--- contentscript.js
+++ contentscript.js
@@ -1,11 +1,9 @@
 let images = new Map();
+const GDOCS_IMG_OBJECT_CLASS = "kix-embeddedobject-image";
+const GDOCS_IMG_CONTAINER_CLASS = "goog-inline-block";
 
 // Check if page is google docs
 // can this be done from the manifest to only apply on googledocs domains?
-
-// Create image menu overlay
-//v1
-// single button to view image in new tab
 
 //v2
 // button to view image in shadowbox
@@ -21,17 +19,26 @@ let images = new Map();
 //   return url.indexOf("docs.google.com") !== -1;
 // }
 
+// Get the image URL from the GDOCS_IMG_OBJECT_CLASS
+function getImageURL(image) {
+  return image.getElementsByTagName("image")[0].getAttribute("xlink:href");
+}
+
+// Insert anchor into parent container
+function insertAnchor(image) {
+  const imageContainer = image.closest(`.${GDOCS_IMG_CONTAINER_CLASS}`);
+  const imageURL = getImageURL(image);
+  const imageHTML = `<a href="${imageURL}" target="_blank" class="imageLink"><p>View Image</p></a>`;
+  imageContainer.insertAdjacentHTML("afterbegin", imageHTML);
+}
+
 // find the images and store the object and image source
 function findImageEmbeds() {
-  const images = document.getElementsByClassName("kix-embeddedobject-image");
+  const images = document.getElementsByClassName(GDOCS_IMG_OBJECT_CLASS);
   const imageSourceURL = images[0]
     .getElementsByTagName("image")[0]
     .getAttribute("xlink:href");
 
-  const imgContainer = images[0].closest(".goog-inline-block");
-  const imgHTML = `<a href="${imageSourceURL}" target="_blank" class="imageLink"><p>View Image</p></a>`;
-  imgContainer.insertAdjacentHTML("afterbegin", imgHTML);
+  insertAnchor(images[0]);
 }
 window.addEventListener("load", () => findImageEmbeds(), false);
-
-// <a href="https://lh4.googleusercontent.com/mYxq77dhBdiScIQ_Y1NqM0uUUdwA3XebNKFvR69qiyysexI_FV3lRtVaHvt1Zl9y8uiZU7qE2vZsHyo_E6FwYfDE2Dki7uCoKPTne7RifoxJhunSY8wyC95GgpS_IodZUZW-PEej" target="_blank" class="imageLink"><p>View Image</p></a>

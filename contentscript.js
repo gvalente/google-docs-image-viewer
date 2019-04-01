5cb022f6cd26c3c8a18699c7c56f7f7a03229b25
Gino Valente <gino.valente3@gmail.com>
2019-04-01 11:38:44 -0700

    Switched to using svg full size icon instead of View Image text

--- contentscript.js
+++ contentscript.js
@@ -5,15 +5,6 @@ const GDOCS_IMG_CONTAINER_CLASS = "goog-inline-block";
 // Check if page is google docs
 // can this be done from the manifest to only apply on googledocs domains?
 
-//v2
-// button to view image in shadowbox
-// import shadowbox
-// button to download image
-
-// Scan for all images within the document
-// get image source
-// append hidden image menu onto image
-
 // check if a url is a google docs
 // function isGDOCurl(url) {
 //   return url.indexOf("docs.google.com") !== -1;
@@ -25,20 +16,19 @@ function getImageURL(image) {
 }
 
 // Insert anchor into parent container
-function insertAnchor(image) {
+function insertImageLinkAnchor(image) {
   const imageContainer = image.closest(`.${GDOCS_IMG_CONTAINER_CLASS}`);
   const imageURL = getImageURL(image);
-  const imageHTML = `<a href="${imageURL}" target="_blank" class="imageLink"><p>View Image</p></a>`;
+  // const imageHTML = `<a href="${imageURL}" target="_blank" class="imageLink"><p>View Image</p></a>`;
+  const imageHTML = `<a href="${imageURL}" target="_blank" class="imageLink"></a>`;
   imageContainer.insertAdjacentHTML("afterbegin", imageHTML);
 }
 
 // find the images and store the object and image source
 function findImageEmbeds() {
-  const images = document.getElementsByClassName(GDOCS_IMG_OBJECT_CLASS);
-  const imageSourceURL = images[0]
-    .getElementsByTagName("image")[0]
-    .getAttribute("xlink:href");
-
-  insertAnchor(images[0]);
+  let images = document.getElementsByClassName(GDOCS_IMG_OBJECT_CLASS);
+  for (let image of images) {
+    insertImageLinkAnchor(image);
+  }
 }
 window.addEventListener("load", () => findImageEmbeds(), false);
